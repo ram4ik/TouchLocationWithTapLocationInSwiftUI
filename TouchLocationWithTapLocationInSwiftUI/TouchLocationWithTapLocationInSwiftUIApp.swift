af98e05acb84e256eec83b94416132a27e3c9ac6
Ramill <ramill_ibragimov@icloud.com>
2022-10-05 13:13:19 +0300

    Initial Commit

---
+++ TouchLocationWithTapLocationInSwiftUI/TouchLocationWithTapLocationInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchLocationWithTapLocationInSwiftUIApp.swift
+//  TouchLocationWithTapLocationInSwiftUI
+//
+//  Created by Ramill Ibragimov on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchLocationWithTapLocationInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
